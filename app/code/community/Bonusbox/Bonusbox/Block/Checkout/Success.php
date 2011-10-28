7400ec6d0e2e3baf9baec42170485fce736c1f21
Christian Dittmar <dittmar@vivomedia.de>
2011-10-28 19:59:32 +0200

    Complete Module

---
+++ app/code/community/Bonusbox/Bonusbox/Block/Checkout/Success.php
@@ -0,0 +1,32 @@
+<?php
+class Bonusbox_Bonusbox_Block_Checkout_Success extends Mage_Core_Block_Template
+{
+	protected $_template = 'bonusbox/checkout/success.phtml';
+	
+	protected function _beforeToHtml()
+	{
+		parent::_beforeToHtml();
+		if (Mage::helper('bonusbox')->isEnabled())
+		{
+			$orderId = Mage::getSingleton('checkout/session')->getLastOrderId();
+			if ($orderId) 
+			{
+				$order = Mage::getModel('sales/order')->load($orderId);
+				if ($order->getId())
+				{
+					$response = Mage::getModel('bonusbox/client')->requestSuccessPage($order);
+					$this->setSuccessPageUrl($response['success_page']['url']);
+				}
+			}
+		}
+	}
+	
+	
+	protected function _toHtml()
+	{
+		if ($this->getSuccessPageUrl())
+		{
+			return parent::_toHtml();
+		}
+	}
+}
---
+++ app/code/community/Bonusbox/Bonusbox/Helper/SuccessPage.php
@@ -0,0 +1,5 @@
+<?php
+class Bonusbox_Bonusbox_Helper_SuccessPage extends Bonusbox_Bonusbox_Helper_Data
+{
+	protected $configSection = 'success_page'; 
+}
